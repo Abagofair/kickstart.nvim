a3024f4d34303b16d6bc6f71e05dd472c488e35e
Anders <anderskoch.airbag@gmail.com>
2026-02-11 18:21:34 +0100

    mmmmmmm color

--- init.lua
+++ init.lua
@@ -110,6 +110,8 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+vim.cmd.colorscheme 'Tomorrow-Night-Eighties'
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -788,27 +790,26 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
+ --{ -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+ --   'folke/tokyonight.nvim',
+ --   priority = 1000, -- Make sure to load this before all the other start plugins.
+ --   config = function()
+ --     ---@diagnostic disable-next-line: missing-fields
+ --    require('tokyonight').setup {
+ --       styles = {
+ --        comments = { italic = false }, -- Disable italics in comments
+ --       },
+ --     }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
+    --end,
+  --},
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
